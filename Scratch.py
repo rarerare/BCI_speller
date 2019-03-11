14f0eb637d4e0d479dc06add7f5faf807148db89
Maylibooyah69 <35562431+Maylibooyah69@users.noreply.github.com>
2019-03-11 13:14:41 -0700

    update

---
+++ Scratch.py
@@ -0,0 +1,8 @@
+import numpy as np"""
+Created on Mon Mar 11 12:37:43 2019
+
+@author: mayli
+"""
+np.array
+f[0]=(137, 1552324497.320355, 0)
+print(f)
---
+++ temp.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import ast
+from scipy.stats import kurtosis
+"""
+Created on Mon Mar 11 12:10:51 2019
+
+@author: mayli
+"""
+f=open("signal_data\signal03_11_2019__10_14_48.txt", "r")
+g=open("stimuli_data\stimuli03_11_2019__10_57_39.txt", "r")
+if f.mode == 'r':
+    contents=f.read()
+    l=ast.literal_eval(contents)
+if g.mode == 'r':
+    content=g.read()
+    gg=ast.literal_eval(content)
+
+#plt.plot([t[0] for t in l])
+aa=np.array(l)
+plt.plot([t[0] for t in aa])
+
+if 1==1 and 2==2:
+    print("yes")
+
+    
